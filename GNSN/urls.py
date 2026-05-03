d9bad0bb2e1afb7a80dda6cddc25789dcc1acf9a
eduardo macOS <eduardocastroemp@gmail.com>
2026-05-02 22:45:08 -0300

    fix: ajuste para rota 404 na raiz -> redirecionado para /gnsn

--- GNSN/urls.py
+++ GNSN/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda request: redirect("gnsn/")),
     path("gnsn/", include("app.urls")),
     path("gnsn/admin/", admin.site.urls),
     path("gnsn/guarda/", include("guarda.urls")),
